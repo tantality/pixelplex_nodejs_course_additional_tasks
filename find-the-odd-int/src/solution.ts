9f2070318eba5d633b38fb17ebb2fd31d22b81d1
tantality <angel.tretyak@mail.ru>
2022-10-20 23:40:14 +0300

    feat: add a solution to the find-the-odd-int challenge

---
+++ find-the-odd-int/src/solution.ts
@@ -0,0 +1,23 @@
+interface INumberCounter{
+  [key:string]:number
+}
+
+export const solve=(arr:number[]):number => getOddInt(getNumberCounter(arr));
+
+function getNumberCounter(arr:number[]):INumberCounter {
+  return arr.reduce((accumulator:INumberCounter,currentNumber:number) => {
+    currentNumber in accumulator? accumulator[currentNumber]+=1 : accumulator[currentNumber]=1;
+    return accumulator;
+  }, {} as INumberCounter);
+}
+
+function getOddInt(numberCounter:INumberCounter):number {
+  let oddInt=0;
+  for (const key in numberCounter) {
+    if (numberCounter[key]%2) {
+      oddInt=Number(key);
+      break;
+    }
+  }
+  return oddInt;
+}
