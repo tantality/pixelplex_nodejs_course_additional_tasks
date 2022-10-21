3952ed8ab08813ff644f567de1eed5c606f116f5
tantality <angel.tretyak@mail.ru>
2022-10-21 22:56:43 +0300

    feat: implement the ability to enter an input parameters for a function and add validation of the input data (reverse-or-rotate)

--- reverse-or-rotate/src/index.ts
+++ reverse-or-rotate/src/index.ts
@@ -1,4 +1,28 @@
+import { Command } from 'commander';
 import { solve } from './solution';
 
-// eslint-disable-next-line no-console
-console.log(solve('123456987654',6));
+const program = new Command();
+
+const canStringBeInt=(str:string):boolean => Array.from(str).every(char => canCharBeInt(char));
+const canCharBeInt=(char:string):boolean => Number.isInteger(Number(char));
+const showMessage=(msg:string):void => {
+  // eslint-disable-next-line no-console
+  console.log(msg);
+};
+
+program
+  .version('1.0.0', '-v, --version')
+  .requiredOption('-str, --string <value>', 'a string consisting of integers')
+  .requiredOption('-sz, --size <value>', 'the number that is the length of the fragments')
+  .action(({ string,size }) => {
+    if (!canCharBeInt(size) || !size) {
+      return showMessage('-sz (--size) must be an integer greater than 0');
+    }
+    if (canStringBeInt(string)) {
+      return showMessage(`reverse and rotate result is ${solve(string,size)}`);
+    } else {
+      return showMessage('-str (--string). Please enter a string consisting of integers');
+    }
+  })
+  .parse(process.argv);
+
